75dc43d8b1a75341646c29d9297587da2913f52f
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2024-02-26 07:20:58 +0530

    Add handling for no results in Path component

--- components/Path.tsx
+++ components/Path.tsx
@@ -9,6 +9,17 @@ function Path({
 		second: string
 	}[]
 }) {
+	const first = steps[0].first
+	const second = steps[0].second
+	const result = steps[0].result
+	if (first === undefined || second === undefined || result === undefined)
+		return (
+			<div className="flex justify-center mt-4">
+				<div className="p-12 flex flex-col gap-6 justify-center border rounded-lg">
+					<h2 className="text-2xl font-bold">No Results</h2>
+				</div>
+			</div>
+		)
 	return (
 		<div className="flex justify-center mt-4">
 			<div className="p-12 flex flex-col gap-6 justify-center border rounded-lg">
