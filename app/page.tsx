912e04e883ae4d943d51668cf12c56e2bd240244
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2024-02-28 17:11:32 +0530

    Add runtime variable to App component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react'
 import Results from '@/components/Results'
 import { SearchBar } from '../components/SearchBar'
-
+export const runtime = 'nodejs'
 export default async function App({ params }: { params: any }) {
 	const item = params ? params?.item?.replace(/%20/g, ' ') : null
 	console.log('hello', process.env.VERCEL_URL)
