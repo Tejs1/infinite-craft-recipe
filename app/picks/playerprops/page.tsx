7d3af3cdf9fd548a223d2c1220b159b534411c83
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2024-03-03 23:37:07 +0530

    Update import statement in table.tsx

--- app/picks/playerprops/page.tsx
+++ app/picks/playerprops/page.tsx
@@ -1,63 +1,63 @@
-import { Payment, columns } from './columns'
-import { DataTable } from './data-table'
+// import { Payment, columns } from './columns'
+// import { DataTable } from './data-table'
 
-async function getData(): Promise<Payment[]> {
-	// Fetch data from your API here.
-	return [
-		{
-			id: '728ed52f',
-			name: 'mike',
-			'Consensus Line': 'mike',
-			Diff: 100,
-			Points: 100,
-			avgSQPoints: 100,
-			MatchDifficulty: 100,
-			'Projected Defensive Matchup': 'mike',
-			'Matchup Insight': 'loreum ipsum dolar sit amet  ',
-		},
-		{
-			id: '728ed52f',
-			name: 'mike',
-			'Consensus Line': 'mike',
-			Diff: 100,
-			Points: 100,
-			avgSQPoints: 100,
-			MatchDifficulty: 100,
-			'Projected Defensive Matchup': 'mike',
-			'Matchup Insight': 'loreum ipsum dolar sit amet  ',
-		},
-		{
-			id: '728ed52f',
-			name: 'mike',
-			'Consensus Line': 'mike',
-			Diff: 100,
-			Points: 100,
-			avgSQPoints: 100,
-			MatchDifficulty: 100,
-			'Projected Defensive Matchup': 'mike',
-			'Matchup Insight': 'loreum ipsum dolar sit amet  ',
-		},
-		{
-			id: '728ed52f',
-			name: 'mike',
-			'Consensus Line': 'mike',
-			Diff: 100,
-			Points: 100,
-			avgSQPoints: 100,
-			MatchDifficulty: 100,
-			'Projected Defensive Matchup': 'mike',
-			'Matchup Insight': 'loreum ipsum dolar sit amet  ',
-		},
-		// ...
-	]
-}
+// async function getData(): Promise<Payment[]> {
+// 	// Fetch data from your API here.
+// 	return [
+// 		{
+// 			id: '728ed52f',
+// 			name: 'mike',
+// 			'Consensus Line': 'mike',
+// 			Diff: 100,
+// 			Points: 100,
+// 			avgSQPoints: 100,
+// 			MatchDifficulty: 100,
+// 			'Projected Defensive Matchup': 'mike',
+// 			'Matchup Insight': 'loreum ipsum dolar sit amet  ',
+// 		},
+// 		{
+// 			id: '728ed52f',
+// 			name: 'mike',
+// 			'Consensus Line': 'mike',
+// 			Diff: 100,
+// 			Points: 100,
+// 			avgSQPoints: 100,
+// 			MatchDifficulty: 100,
+// 			'Projected Defensive Matchup': 'mike',
+// 			'Matchup Insight': 'loreum ipsum dolar sit amet  ',
+// 		},
+// 		{
+// 			id: '728ed52f',
+// 			name: 'mike',
+// 			'Consensus Line': 'mike',
+// 			Diff: 100,
+// 			Points: 100,
+// 			avgSQPoints: 100,
+// 			MatchDifficulty: 100,
+// 			'Projected Defensive Matchup': 'mike',
+// 			'Matchup Insight': 'loreum ipsum dolar sit amet  ',
+// 		},
+// 		{
+// 			id: '728ed52f',
+// 			name: 'mike',
+// 			'Consensus Line': 'mike',
+// 			Diff: 100,
+// 			Points: 100,
+// 			avgSQPoints: 100,
+// 			MatchDifficulty: 100,
+// 			'Projected Defensive Matchup': 'mike',
+// 			'Matchup Insight': 'loreum ipsum dolar sit amet  ',
+// 		},
+// 		// ...
+// 	]
+// }
 
-export default async function DemoPage() {
-	const data = await getData()
+// export default async function DemoPage() {
+// 	const data = await getData()
 
-	return (
-		<div className="container mx-auto py-10">
-			<DataTable columns={columns} data={data} />
-		</div>
-	)
-}
+// 	return (
+// 		<div className="container mx-auto py-10">
+// 			<DataTable columns={columns} data={data} />
+// 		</div>
+// 	)
+// }
